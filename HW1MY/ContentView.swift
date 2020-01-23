605dd3b65f99532b0bb04d71d0653f16a31797cb
aliakseik <koran.aliaksei@yandex.by>
2020-01-23 11:21:30 -0800

    try more

---
+++ HW1MY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1MY
+//
+//  Created by Ульяна Сексишмекси on 23.01.2020.
+//  Copyright © 2020 Ульяна Сексишмекси. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
